e99564c97ad012009f3f5c146df671fcc4b58a24
Xavier JAFFREZIC <xavier.jaffrezic@renault.com>
2016-05-23 17:00:05 +0200

    Added rough code for file read/write

--- app/src/main/java/com/dustofcloud/daytodayrace/FileReader.java
+++ app/src/main/java/com/dustofcloud/daytodayrace/FileReader.java
@@ -1,11 +1,20 @@
 package com.dustofcloud.daytodayrace;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
 public class FileReader {
     EventsFileReader Notify = null;
 
     public FileReader(int DaysBackLimit, EventsFileReader LoaderClient ) {
         Notify = LoaderClient;
+        FileInputStream fis = context.openFileInput(REGION_FILENAME);
+        ObjectInputStream is = new ObjectInputStream(fis);
+        Object readObject = is.readObject();
+        is.close();
 
+        if(readObject != null && readObject instanceof Region) {
+            return (Region) readObject;
     }
 
 
--- app/src/main/java/com/dustofcloud/daytodayrace/FileWriter.java
+++ app/src/main/java/com/dustofcloud/daytodayrace/FileWriter.java
@@ -1,10 +1,19 @@
 package com.dustofcloud.daytodayrace;
 
+import android.content.Context;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 public class FileWriter {
     public FileWriter() {
     }
 
     public void WriteWaypoint(WayPoint WaypointtoWrite) {
-
+        FileOutputStream fos = context.openFileOutput(REGION_FILENAME, Context.MODE_PRIVATE);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(region);
+        oos.close();
     }
 }
+
