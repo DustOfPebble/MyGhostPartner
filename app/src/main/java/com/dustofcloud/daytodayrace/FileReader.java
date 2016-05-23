bf354cd6b2ece82fa49207cc21d8f9a024be3cc2
Xavier JAFFREZIC <xavier.jaffrezic@renault.com>
2016-05-23 17:20:08 +0200

    Read/Write changes ...

--- app/src/main/java/com/dustofcloud/daytodayrace/FileReader.java
+++ app/src/main/java/com/dustofcloud/daytodayrace/FileReader.java
@@ -3,19 +3,26 @@ package com.dustofcloud.daytodayrace;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 
-public class FileReader {
+public class FileReader implements Runnable {
     EventsFileReader Notify = null;
 
     public FileReader(int DaysBackLimit, EventsFileReader LoaderClient ) {
         Notify = LoaderClient;
         FileInputStream fis = context.openFileInput(REGION_FILENAME);
         ObjectInputStream is = new ObjectInputStream(fis);
+    }
+
+    @Override
+    public void run() {
+        // Moves the current Thread into the background
+        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
+
         Object readObject = is.readObject();
         is.close();
 
-        if(readObject != null && readObject instanceof Region) {
-            return (Region) readObject;
+        if (readObject != null && readObject instanceof WayPoint) {
+            Notify.WaypointLoaded((WayPoint) readObject);
+
+        }
     }
-
-
 }
