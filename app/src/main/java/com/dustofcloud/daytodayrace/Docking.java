833eba871f1b03e26164df61bf4800ea2bdc5c98
Xavier JAFFREZIC <xavier.jaffrezic@gmail.com>
2016-05-30 23:04:07 +0200

    intantiate ControlSwitch in Docking ...

--- app/src/main/java/com/dustofcloud/daytodayrace/Docking.java
+++ app/src/main/java/com/dustofcloud/daytodayrace/Docking.java
@@ -8,12 +8,15 @@ import android.os.Bundle;
 public class Docking extends Activity {
 
     private PointsDrawer PointsViewer = null;
+    private ControlSwitch SleepLocker = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dock_views);
         PointsViewer = (PointsDrawer) findViewById(R.id.show_waypoints);
+        SleepLocker = (ControlSwitch) findViewById(R.id.ButtonWiFiStatus);
+
     }
 
 
