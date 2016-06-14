7fadc7a2e3888238bfb6a0f330a7119372d3ce32
Xavier JAFFREZIC <xavier@jaffrezic.net>
2016-06-14 22:41:59 +0200

    implementing Frame draw ...

--- app/src/main/java/com/dustofcloud/daytodayrace/Monitor.java
+++ app/src/main/java/com/dustofcloud/daytodayrace/Monitor.java
@@ -3,6 +3,8 @@ package com.dustofcloud.daytodayrace;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.util.Pair;
 import android.widget.ImageView;
@@ -12,6 +14,14 @@ import java.util.ArrayList;
 public class Monitor extends ImageView {
     private ArrayList<Statistic> Collected;
     private float MeanValue;
+    private Path MonitorFrame;
+    private Path MonitorFrameFilled;
+    private Paint FrameBorderPainter;
+    private Paint FrameBackgroundPainter;
+    private static final int FrameBorderColor = 0xff84e9f4;
+    private static final int FrameBackgroundColor = 0xff00bebe;
+    private static final float FrameBorderThickness = 5f;
+
 
     public Monitor(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -28,7 +38,17 @@ public class Monitor extends ImageView {
         finally { attributes.recycle();}
 
 //        this.setImageDrawable(highIcon);
-        invalidate();
+        FrameBorderPainter = new Paint();
+        FrameBorderPainter.setStyle(Paint.Style.STROKE);
+        FrameBorderPainter.setColor(FrameBorderColor);
+        FrameBorderPainter.setStrokeWidth(FrameBorderThickness);
+
+        FrameBackgroundPainter = new Paint();
+        FrameBackgroundPainter.setStyle(Paint.Style.FILL);
+        FrameBorderPainter.setColor(FrameBackgroundColor);
+        MonitorFrame = new Path();
+        MonitorFrameFilled = MonitorFrame;
+
     }
 
     public  void updateStatistics(ArrayList<Statistic> values) {
@@ -46,11 +66,26 @@ public class Monitor extends ImageView {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
     {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int Width = MeasureSpec.getSize(widthMeasureSpec);
+        int Height = MeasureSpec.getSize(heightMeasureSpec);
+        this.setMeasuredDimension(Width, Height);
+        if ((Width == 0) || (Height == 0)) return;
+        MonitorFrame.moveTo(0,0);
+        MonitorFrame.lineTo(Width,0);
+        MonitorFrame.lineTo(Width,Height);
+        MonitorFrame.lineTo(0,Height);
+        MonitorFrame.close();
+        MonitorFrameFilled = new Path(MonitorFrame);
+        MonitorFrameFilled.setFillType(Path.FillType.WINDING);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
         super.onDraw(canvas);
+        canvas.drawPath(MonitorFrameFilled,FrameBackgroundPainter);
+        canvas.drawPath(MonitorFrame,FrameBorderPainter);
+
     }
 }
